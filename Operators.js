5349789e82f08a2b2461e4674647e30b77112f58
Mahesh Langote <maheshlangote777@gmail.com>
2023-09-27 23:38:55 +0530

    Operator

---
+++ Operators.js
@@ -0,0 +1,11 @@
+let num1 = 10;
+let num2 = 5;
+
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
+console.log("Modulus:", num1 % num2);
+
+let isGreaterThan = num1 > num2;
+console.log("Is num1 greater than num2?", isGreaterThan);
